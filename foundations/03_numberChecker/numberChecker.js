9fea6f0fcda8dd3578dc30526d1806f0a9c458ab
Connor <78576854+ProgrammingPaddy@users.noreply.github.com>
2026-04-30 20:38:27 -0400

    Complete exercise 03 in foundations

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,9 +1,8 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number > 9){
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Do not edit below this line
