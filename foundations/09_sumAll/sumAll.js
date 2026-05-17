a006203967d5e65aa65f46b4c6b021c4a1e7b2d1
connor <78576854+ProgrammingPaddy@users.noreply.github.com>
2026-05-16 20:45:41 -0400

    Complete exercise 09 for loops

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
 
+    let high = Math.max(num1, num2);
+    let low = Math.min(num1, num2);
+    let newNum = 0;
+    for (let i = low; i <= high; i++){
+        newNum += i;
+    }
+    return newNum;
 };
 
 // Do not edit below this line
